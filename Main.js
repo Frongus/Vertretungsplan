5943407a8482892cfa104f83d8094d4c87f6014a
Frongus <pommes.exe.georg@gmail.com>
2022-07-26 20:19:23 +0200

    2

--- Main.js
+++ Main.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const server = express();
 const env = require('env');
-const config = './config/config.json';
+const config = require('./config/config.json');
+const fs = require('fs');
+const os = require('os');
 
 Startup();
 
 function Startup() {
-    console.log(config);
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log(os.arch() + ' | ' + os.version() + ' | ' + os.platform());
+    console.log("-----------------------------------------------------------------------------------------------------");
+    console.log("-----------------------------------------------------------------------------------------------------");
+    console.log("");
+    console.log('Student-Access-key: ' + config.STUDENT);
+    console.log('Admin-Access-key: ' + config.ADMINISTATOR);
+    console.log('Web-Server-Port: ' + config.PORT);
+    server.listen(config.PORT, (err) => {if(err){console.log(err)}else{console.log("Webserver online")}});
+    console.log("");
+    console.log("-----------------------------------------------------------------------------------------------------");
+    console.log("-----------------------------------------------------------------------------------------------------");
 }
