de26b1b4320eba4bc97a73bf8e898fe53f0ba2fc
Frongus <pommes.exe.georg@gmail.com>
2022-07-26 20:25:28 +0200

    3

--- Main.js
+++ Main.js
@@ -2,10 +2,11 @@ const express = require('express');
 const server = express();
 const env = require('env');
 const config = require('./config/config.json');
-const fs = require('fs');
 const os = require('os');
 
 Startup();
+
+server.use('/', require('./routes/pages.js'))
 
 function Startup() {
     console.log("");
@@ -25,6 +26,7 @@ function Startup() {
     console.log('Student-Access-key: ' + config.STUDENT);
     console.log('Admin-Access-key: ' + config.ADMINISTATOR);
     console.log('Web-Server-Port: ' + config.PORT);
+    console.log(__dirname);
     server.listen(config.PORT, (err) => {if(err){console.log(err)}else{console.log("Webserver online")}});
     console.log("");
     console.log("-----------------------------------------------------------------------------------------------------");
