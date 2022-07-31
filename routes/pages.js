c86abc28f22e137fa37323902c895b6463543200
Frongus <110054294+Frongus@users.noreply.github.com>
2022-07-31 17:36:28 +0200

    pageUpdate

--- routes/pages.js
+++ routes/pages.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const config = require('./config/config.json');
 
 router.get('/', (req,res) => {
     console.log(`${req.ip} requested ${req.url}`);
@@ -7,8 +8,25 @@ router.get('/', (req,res) => {
 });
 
 router.post('/Plan/', (req,res) => {
+    var ReqData = req.body;
+    console.log(`Request has been made: ${ReqData}`);
+    
+    if(ReqData.ID == config.STUDENT)
+    {
+        res.render('Plan.hbs');
+    }
+    else
+    {
+        if(ReqData.ID == config.ADMINISTATOR)
+        {
+            res.render('Adminpannel.hbs');
+        }
+        else
+        {
+            res.send("error, wrong Student/Admin ID");
+        }
+    }
     console.log(`${req.ip} requested ${req.url}`);
-    res.render('Plan.hbs');
 });
 
 module.exports = router;
